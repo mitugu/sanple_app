0ce2532f5f7f2e8a0ab83f48d595af9652ec0789
mitsugu <8964inp.step@gmail.com>
2019-03-03 11:55:22 +0900

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def render htmkl:"hello,world"
+  def hello
+    render html:"hello,world"
   end
 end
