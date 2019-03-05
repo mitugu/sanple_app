333afaaf3bcc8affbf47685e181ff618e135bf9e
mitsugu <8964inp.step@gmail.com>
2019-03-05 21:41:08 +0900

    Improve the README

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+def home
+
+end
+
   def hello
     render html:"hello,world"
+  end
+  def about
   end
 end
